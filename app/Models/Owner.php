27286c692dd38da3522ba092f85fe3c472fb43d0
Kate Fox <katefox100@gmail.com>
2021-02-17 13:03:39 +0000

    fullName method working on Owner Model

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -11,4 +11,9 @@ class Owner extends Model
 
     protected $fillable = ["first_name", "last_name", "telephone", "email", "address_1", "address_2", "town", "postcode"];
 
+    public function fullName()
+    {
+        return ("{$this->first_name} {$this->last_name}");
+    }
+
 }
