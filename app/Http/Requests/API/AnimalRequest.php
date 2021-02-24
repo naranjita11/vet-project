4b0c863775b9fb7f05b46191a1c62184c55c5722
Kate Fox <katefox100@gmail.com>
2021-02-24 10:50:48 +0000

    validation rules set on AnimalRequest.php

---
+++ app/Http/Requests/API/AnimalRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\API;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class AnimalRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "name" => ["required", "string", "max:100"],
+            "type" => ["required", "string", "max:100"],
+            "date_of_birth" => ["required", "date"],
+            "weight" => ["required", "numeric"],
+            "height" => ["required", "numeric"],
+            "biteyness" => ["required", "numeric", "max:5"],
+          ];
+    }
+}
